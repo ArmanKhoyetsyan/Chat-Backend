f860aa10f94e06b02c6a87cb6ff68042f76572d0
Arman <95692425+ArmanKhoyetsyan@users.noreply.github.com>
2022-09-02 17:49:47 +0400

    Update db.request.js

--- db.request.js
+++ db.request.js
@@ -1,12 +1,9 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: 'llcrttvkfzvpmg',
-    host: 'ec2-44-210-36-247.compute-1.amazonaws.com',
-    database: 'd6992gst9gbcq4',
-    password: '4860590a968ddb81b305ab45d27ac1d05bc8e23db339dbff9b7b8c9cdd768352',
-    port: '5432',
+    url: process.env.DATABASE_URL
 });
+
 
 const getUsers = async () => {
     try {
