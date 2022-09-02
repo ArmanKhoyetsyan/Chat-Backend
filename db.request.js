50c29bca3da65c61e150dbc9a51571b736372987
Arman <95692425+ArmanKhoyetsyan@users.noreply.github.com>
2022-09-02 17:52:38 +0400

    Update db.request.js

--- db.request.js
+++ db.request.js
@@ -4,6 +4,14 @@ const pool = new Pool({
     url: process.env.DATABASE_URL
 });
 
+// const pool = new Pool({
+//     user: 'llcrttvkfzvpmg',
+//     host: 'ec2-44-210-36-247.compute-1.amazonaws.com',
+//     database: 'd6992gst9gbcq4',
+//     password: '4860590a968ddb81b305ab45d27ac1d05bc8e23db339dbff9b7b8c9cdd768352',
+//     port: '5432',
+// });
+
 
 const getUsers = async () => {
     try {
